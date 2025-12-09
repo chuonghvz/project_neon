adcbe8206949dab9e9ee2571fdfef5caf109dbe9
Ho Van Chuong <chuonghv96@gmail.com>
2025-12-09 22:10:00 +0700

    Update for cloud deploy

--- src/main/java/com/chuonghv/neno/controller/HelloController.java
+++ src/main/java/com/chuonghv/neno/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.chuonghv.neno.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${constant.app.env:}")
+    private String appEnv;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from GKE CI/CD!";
+        return "Hello from GKE CI/CD from env: " + appEnv;
     }
 }
